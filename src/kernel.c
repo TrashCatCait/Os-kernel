db7ac7065858c7cdd6cea8b7262194849ec5d037
Cailtyn <caitlynrosestewart@gmail.com>
2021-06-06 20:13:20 +0100

    updated kernel and code a small amount

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,14 @@
+typedef struct {
+    int memMapSize;
+    int descriptSize;
+    int mapKey;
+    void* map;
+    int descriptVer;
+} boot_info;
 
-int _start()
+int _start(boot_info BI)
 {
+    char *vidmem = (char*)0xb8000;
+    vidmem[1] = 'h';
     return 1922;
 }
